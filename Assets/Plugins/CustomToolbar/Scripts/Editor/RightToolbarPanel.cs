f75fa54fa154db80ce434c552e6b6c894e56d7b5
Dimka Tabakov <nebo.reka@gmail.com>
2020-07-12 17:44:11 +0300

    Added CustomToolbar and customizer for the project

---
+++ Assets/Plugins/CustomToolbar/Scripts/Editor/RightToolbarPanel.cs
@@ -0,0 +1,30 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace CustomToolbar
+{
+    [InitializeOnLoad]
+    public static class RightToolbarPanel
+    {
+        static RightToolbarPanel()
+        {
+            CustomToolbar.RightToolbarGUI.Add(OnToolbarGUI);
+        }
+        static void OnToolbarGUI()
+        {
+            EditorGUIUtility.SetIconSize(new Vector2(17,17));
+            if (GUILayout.Button("R", ToolbarStyles.commandButtonStyle))
+            {
+                if (EditorApplication.isPlaying)
+                {
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+                }
+            }
+
+            Time.timeScale = EditorGUILayout.Slider("", Time.timeScale, 1, 20,
+                GUILayout.Width(150));
+        }
+    }
+}
